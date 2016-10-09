bc9340db8a3552511746f35c72c81ec7b678f9ba
Maksim Chemerisuk <chemerisuk@gmail.com>
2016-10-09 10:55:41 +0300

    initial implementation

---
+++ src/android/copy-google-services.js
@@ -0,0 +1,70 @@
+module.exports = function (context) {
+
+  var fs = require('fs'),
+      path = require('path');
+
+  function logMe(what) {
+    console.error(what);
+  }
+
+  logMe("Running hook to copy any available google-services file to Android");
+
+  function fileExists(path) {
+    try  {
+      return fs.statSync(path).isFile();
+    } catch (e) {
+      logMe("fileExists error: " + e);
+      return false;
+    }
+  }
+
+  function directoryExists(path) {
+    try  {
+      return fs.statSync(path).isDirectory();
+    } catch (e) {
+      logMe("directoryExists error: " + e);
+      return false;
+    }
+  }
+
+  var androidPlatform = path.join(context.opts.projectRoot, 'platforms/android/');
+  var androidFolder = fs.existsSync(androidPlatform) ? androidPlatform : context.opts.projectRoot;
+
+  if (directoryExists(androidFolder)) {
+    var paths = ["google-services.json", path.join(androidFolder, "assets/www/google-services.json")];
+
+    for (var i = 0; i < paths.length; i++) {
+      if (fileExists(paths[i])) {
+        logMe("Found this file to write to Android: " + paths[i]);
+        try {
+          var contents = fs.readFileSync(paths[i]).toString();
+          fs.writeFileSync(path.join(androidFolder, "google-services.json"), contents);
+
+          var json = JSON.parse(contents);
+          var strings = fs.readFileSync(path.join(androidFolder, "res/values/strings.xml")).toString();
+
+          // strip non-default value
+          strings = strings.replace(new RegExp('<string name="google_app_id">([^\@<]+?)</string>', "i"), '');
+
+          // strip non-default value
+          strings = strings.replace(new RegExp('<string name="google_api_key">([^\@<]+?)</string>', "i"), '');
+
+          // strip empty lines
+          strings = strings.replace(new RegExp('(\r\n|\n|\r)[ \t]*(\r\n|\n|\r)', "gm"), '$1');
+
+          // replace the default value
+          strings = strings.replace(new RegExp('<string name="google_app_id">([^<]+?)</string>', "i"), '<string name="google_app_id">' + json.client[0].client_info.mobilesdk_app_id + '</string>');
+
+          // replace the default value
+          strings = strings.replace(new RegExp('<string name="google_api_key">([^<]+?)</string>', "i"), '<string name="google_api_key">' + json.client[0].api_key[0].current_key + '</string>');
+
+          fs.writeFileSync(path.join(androidFolder, "res/values/strings.xml"), strings);
+        } catch(err) {
+          logMe(err);
+        }
+
+        break;
+      }
+    }
+  }
+};
---
+++ src/ios/copy-google-services.js
@@ -0,0 +1,97 @@
+module.exports = function (context) {
+
+  var fs = require('fs'),
+      path = require('path'),
+      xcode = require('xcode'),
+      plist = require('plist'),
+      util = require('util');
+
+  function logMe(what) {
+    console.error(what);
+  }
+
+  logMe("START Running hook to copy any available google-services file to iOS");
+
+  var getValue = function(config, name) {
+      var value = config.match(new RegExp('<' + name + '>(.*?)</' + name + '>', "i"));
+      if(value && value[1]) {
+          return value[1];
+      } else {
+          return null;
+      }
+  };
+
+  function fileExists(path) {
+    try  {
+      return fs.statSync(path).isFile();
+    } catch (e) {
+      logMe("fileExists error: " + e);
+      return false;
+    }
+  }
+
+  function directoryExists(path) {
+    try  {
+      return fs.statSync(path).isDirectory();
+    } catch (e) {
+      logMe("directoryExists error: " + e);
+      return false;
+    }
+  }
+
+  function addResourceToXcodeProject(resourceName, projName, projFolder, iosFolder) {
+    var projectPath = path.join(projFolder, 'project.pbxproj');
+    var pbxProject;
+    if (context.opts.cordova.project) {
+      pbxProject = context.opts.cordova.project.parseProjectFile(context.opts.projectRoot).xcode;
+    } else {
+      pbxProject = xcode.project(projectPath);
+      pbxProject.parseSync();
+    }
+
+    pbxProject.addResourceFile(resourceName);
+
+    fs.writeFileSync(projectPath, pbxProject.writeSync());
+  }
+
+  var iosPlatform = path.join(context.opts.projectRoot, 'platforms/ios/');
+  var iosFolder = fs.existsSync(iosPlatform) ? iosPlatform : context.opts.projectRoot;
+
+  var data = fs.readdirSync(iosFolder);
+  var projFolder;
+  var projName;
+  if (data && data.length) {
+    data.forEach(function (folder) {
+      if (folder.match(/\.xcodeproj$/)) {
+        projFolder = path.join(iosFolder, folder);
+        projName = path.basename(folder, '.xcodeproj');
+      }
+    });
+  }
+  if (!projFolder || !projName) {
+    throw new Error("Could not find an .xcodeproj folder in: " + iosFolder);
+  }
+
+  if (directoryExists(iosFolder)) {
+    var paths = ["GoogleService-Info.plist", path.join(iosFolder, "www", "GoogleService-Info.plist")];
+
+    for (var i = 0; i < paths.length; i++) {
+      if (fileExists(paths[i])) {
+        try {
+          var contents = fs.readFileSync(paths[i]).toString();
+          logMe("Found this file to write to iOS: " + paths[i]);
+          var destFolder = path.join(iosFolder, projName, "Resources");
+          if (!fs.existsSync(destFolder)) {
+            fs.mkdirSync(destFolder);
+          }
+          fs.writeFileSync(destFolder + "/GoogleService-Info.plist", contents);
+          addResourceToXcodeProject("GoogleService-Info.plist", projName, projFolder, iosFolder);
+        } catch (err2) {
+          logMe(err2);
+        }
+        break;
+      }
+    }
+  }
+  logMe("END Running hook to copy any available google-services file to iOS");
+};
