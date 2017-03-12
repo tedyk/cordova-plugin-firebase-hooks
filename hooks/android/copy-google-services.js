416510751ca024efa2db10ec8755f163ce210d1e
Maksim Chemerisuk <chemerisuk@users.noreply.github.com>
2017-03-12 18:38:55 +0300

    Update copy-google-services.js

--- hooks/android/copy-google-services.js
+++ hooks/android/copy-google-services.js
@@ -44,19 +44,13 @@ module.exports = function (context) {
           var strings = fs.readFileSync(path.join(androidFolder, "res/values/strings.xml")).toString();
 
           // strip non-default value
-          strings = strings.replace(new RegExp('<string name="google_app_id">([^\@<]+?)</string>', "i"), '');
+          strings = strings.replace(new RegExp('<string name="google_app_id">[^<]+?</string>', "i"), '<string name="google_app_id">' + json.client[0].client_info.mobilesdk_app_id + '</string>');
 
           // strip non-default value
-          strings = strings.replace(new RegExp('<string name="google_api_key">([^\@<]+?)</string>', "i"), '');
+          strings = strings.replace(new RegExp('<string name="google_api_key">[^<]+?</string>', "i"), '<string name="google_api_key">' + json.client[0].api_key[0].current_key + '</string>');
 
           // strip empty lines
           strings = strings.replace(new RegExp('(\r\n|\n|\r)[ \t]*(\r\n|\n|\r)', "gm"), '$1');
-
-          // replace the default value
-          strings = strings.replace(new RegExp('<string name="google_app_id">([^<]+?)</string>', "i"), '<string name="google_app_id">' + json.client[0].client_info.mobilesdk_app_id + '</string>');
-
-          // replace the default value
-          strings = strings.replace(new RegExp('<string name="google_api_key">([^<]+?)</string>', "i"), '<string name="google_api_key">' + json.client[0].api_key[0].current_key + '</string>');
 
           fs.writeFileSync(path.join(androidFolder, "res/values/strings.xml"), strings);
         } catch(err) {
